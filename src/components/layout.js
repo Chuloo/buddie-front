d16cb0a3c9e046fec0127815e02ab3010824912b
home <home@Chuloos-MBP.lan>
2019-05-28 15:12:13 +0400

    splash page

---
+++ src/components/layout.js
@@ -0,0 +1,82 @@
+import React from "react"
+import "./layout.css"
+import Logo from "../../hackmamba-logo.png"
+
+const Layout = ({ children }) => {
+  return (
+    <div className="app">
+      <nav
+        className="navbar has-text-weight-bold"
+        role="navigation"
+        aria-label="main navigation"
+      >
+        <div className="navbar-brand">
+          <a className="navbar-item" href="#">
+            <img
+              src={Logo}
+              width="20"
+              height="20"
+              style={{ marginRight: "0.4em" }}
+            />
+            <h4 className="has-text-weight-bold is-size-5">ackmamba</h4>
+          </a>
+
+          <a
+            role="button"
+            className="navbar-burger burger"
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbarBasicExample"
+          >
+            <span aria-hidden="true" />
+            <span aria-hidden="true" />
+            <span aria-hidden="true" />
+          </a>
+        </div>
+
+        <div id="navbarBasicExample" className="navbar-menu">
+          <div className="navbar-end">
+            <a className="navbar-item">Home</a>
+            <a className="navbar-item">Learn</a>
+            <a className="navbar-item">Build</a>
+            <a className="navbar-item">Opportunities</a>
+
+            <div className="navbar-item has-dropdown is-hoverable">
+              <a className="navbar-link">More</a>
+
+              <div className="navbar-dropdown">
+                <a className="navbar-item">About</a>
+                <a className="navbar-item">Contact</a>
+                <hr className="navbar-divider" />
+                <a className="navbar-item">Blog</a>
+              </div>
+              <a className="navbar-item">
+                <span className="icon">
+                  <i className="far fa-user" />
+                </span>
+              </a>
+            </div>
+          </div>
+
+          {/* User Login and Logout */}
+          {/* <div className="navbar-end">
+            <div className="navbar-item">
+              <span className="icon">
+                <i className="fas fa-user" />
+              </span>
+              <div className="buttons">
+                <a className="button is-primary">
+                  <strong>Sign up</strong>
+                </a>
+                <a className="button is-light">Log in</a>
+              </div>
+            </div>
+          </div> */}
+        </div>
+      </nav>
+      {children}
+    </div>
+  )
+}
+
+export default Layout
