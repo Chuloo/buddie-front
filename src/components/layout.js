4e43e732804bc29fa84f4d20ec043edc4f27b609
home <home@Chuloos-MBP.lan>
2019-05-28 18:07:28 +0400

    update layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -49,6 +49,7 @@ const Layout = ({ children }) => {
                 <a className="navbar-item">Contact</a>
                 <hr className="navbar-divider" />
                 <a className="navbar-item">Blog</a>
+                <a className="navbar-item">Stories</a>
               </div>
             </div>
             <a className="navbar-item">
@@ -57,21 +58,6 @@ const Layout = ({ children }) => {
               </span>
             </a>
           </div>
-
-          {/* User Login and Logout */}
-          {/* <div className="navbar-end">
-            <div className="navbar-item">
-              <span className="icon">
-                <i className="fas fa-user" />
-              </span>
-              <div className="buttons">
-                <a className="button is-primary">
-                  <strong>Sign up</strong>
-                </a>
-                <a className="button is-light">Log in</a>
-              </div>
-            </div>
-          </div> */}
         </div>
       </nav>
       {children}
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,60 +6,63 @@ import Art from "../../assets/Artwork@1x.png"
 const Home = () => {
   return (
     <Layout>
-      <div className="section splash_page width">
-        <div className="columns">
-          <div className="column call-to-action is-two-fifths">
-            <h1 className="header__title is-size-1 has-text-weight-bold">
-              Learn. Build. Opportunities.
-            </h1>
-            <p className="header__subtitle">
-              Hackmamba is a community of enthusiasts learning about technology,
-              building along the way, and getting access to relevant
-              opportunities.
-            </p>
-            <div className="action">
-              <button className="button cta_button is-primary is-large">
-                <h1 className="is-size-6">Get early access</h1>
-              </button>
+      <div className="bucket">
+        <div className="section splash_page width">
+          <div className="columns">
+            <div className="column call-to-action is-two-fifths">
+              <h1 className="header__title is-size-1 has-text-weight-bold">
+                Learn. Build. Opportunities.
+              </h1>
+              <p className="header__subtitle">
+                Hackmamba is a community of enthusiasts learning about
+                technology, building along the way, and getting access to
+                relevant opportunities.
+              </p>
+              <div className="action">
+                <button className="button cta_button is-primary is-large">
+                  <h1 className="is-size-6">Get early access</h1>
+                </button>
+              </div>
+            </div>
+            <div className="column illustration">
+              <img
+                src={Art}
+                alt="Illustration"
+                width="500px"
+                className="is-centered"
+              />
             </div>
           </div>
-          <div className="column illustration">
-            <img
-              src={Art}
-              alt="Illustration"
-              width="500px"
-              className="is-centered"
-            />
-          </div>
         </div>
-      </div>
-      <div className="width splash_details columns">
-        <div className="column columns is-6 is-offset-6">
-          <div className="column">
-            <h1 className="has-text-weight-bold is-size-7 header__title">
-              Learn Online
-            </h1>
-            <p className="is-size-7">
-              Learn online using quality content. Take assessments to level up.
-            </p>
-          </div>
-          <div className="column">
-            <h1 className="has-text-weight-bold is-size-7 header__title">
-              Build Projects
-            </h1>
-            <p className="is-size-7">
-              Build projects, contibute to open source and get support of
-              mentors.
-            </p>
-          </div>
-          <div className="column">
-            <h1 className="has-text-weight-bold is-size-7 header__title">
-              Access Opportunities
-            </h1>
-            <p className="is-size-7">
-              Get access to job and entrepreneurship opportunities across the
-              globe.
-            </p>
+        <div className="width splash_details columns">
+          <div className="column columns is-6 is-offset-6">
+            <div className="column">
+              <h1 className="has-text-weight-bold is-size-7 header__title">
+                Learn Online
+              </h1>
+              <p className="is-size-7">
+                Learn online using quality content. Take assessments to level
+                up.
+              </p>
+            </div>
+            <div className="column">
+              <h1 className="has-text-weight-bold is-size-7 header__title">
+                Build Projects
+              </h1>
+              <p className="is-size-7">
+                Build projects, contibute to open source and get support from
+                mentors.
+              </p>
+            </div>
+            <div className="column">
+              <h1 className="has-text-weight-bold is-size-7 header__title">
+                Access Opportunities
+              </h1>
+              <p className="is-size-7">
+                Get access to job and entrepreneurship opportunities across the
+                globe.
+              </p>
+            </div>
           </div>
         </div>
       </div>
